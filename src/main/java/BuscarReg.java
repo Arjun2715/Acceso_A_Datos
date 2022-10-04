8fddf39d80efa44b98c9f428737558338ee536c0
Alumno <arjunjot18@gmail.com>
2022-10-04 13:31:24 +0200

    1

--- src/main/java/BuscarReg.java
+++ src/main/java/BuscarReg.java
@@ -1,2 +1,24 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class BuscarReg {
+    public static void main(String[] args){
+
+
+        /*try {
+            FileReader output = new FileReader("D:\\Nueva carpeta\\AAD_jVera\\Actividad1\\prueba.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+*/
+
+        try {
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
+
 }
